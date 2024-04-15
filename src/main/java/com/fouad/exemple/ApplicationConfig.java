c0d58fc864e0ffbf8326207e7d1b0f5cd6fd3617
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-15 13:25:23 +0100

    Using @Qualifier and naming Beans

--- src/main/java/com/fouad/exemple/ApplicationConfig.java
+++ src/main/java/com/fouad/exemple/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.fouad.exemple;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,11 +8,13 @@ import org.springframework.context.annotation.Configuration;
 public class ApplicationConfig {
 
     @Bean  // Transform Class to Bean
+    @Qualifier("bean1")
     public MyFirstClass myFirstBean(){
         return new  MyFirstClass("First Bean");
     }
 
     @Bean  // Transform Class to Bean
+    @Qualifier("bean2")
     public MyFirstClass mySecondBean(){
         return new  MyFirstClass("Second Bean");
     }
--- src/main/java/com/fouad/exemple/MyFirstService.java
+++ src/main/java/com/fouad/exemple/MyFirstService.java
@@ -1,6 +1,7 @@
 package com.fouad.exemple;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,7 +9,7 @@ public class MyFirstService {
 
     private final MyFirstClass myFirstClass;
 
-    public MyFirstService(MyFirstClass myFirstClass) { //Constructor injection
+    public MyFirstService( @Qualifier("bean2") MyFirstClass myFirstClass) { //Constructor injection
         this.myFirstClass = myFirstClass;
     }
 
