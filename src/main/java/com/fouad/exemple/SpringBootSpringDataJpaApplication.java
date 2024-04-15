1030b30ca39781d5edbf7691135bc9fa30eaff30
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-15 12:47:12 +0100

    Transform the class to a simple Bean

--- src/main/java/com/fouad/exemple/SpringBootSpringDataJpaApplication.java
+++ src/main/java/com/fouad/exemple/SpringBootSpringDataJpaApplication.java
@@ -2,17 +2,24 @@ package com.fouad.exemple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootSpringDataJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSpringDataJpaApplication.class, args);
+		var ctx = SpringApplication.run(SpringBootSpringDataJpaApplication.class, args);
 
-		MyFirstClass myFirstClass = new MyFirstClass();
+		MyFirstClass myFirstClass = ctx.getBean(MyFirstClass.class); //get the bean
 		System.out.println(myFirstClass.sayHello());
 
 
 	}
+
+	@Bean  // Transform Class to Bean
+	public MyFirstClass myFirstClass(){
+		return new  MyFirstClass();
+	}
+
 
 }
