8737a91486b797438461234c5b4ecdd747a9373c
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-15 13:20:32 +0100

    Using two beans with the same name

--- src/main/java/com/fouad/exemple/ApplicationConfig.java
+++ src/main/java/com/fouad/exemple/ApplicationConfig.java
@@ -5,8 +5,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration // The spring boot will scan all the beans in the configuration class
 public class ApplicationConfig {
+
     @Bean  // Transform Class to Bean
     public MyFirstClass myFirstBean(){
         return new  MyFirstClass("First Bean");
     }
+
+    @Bean  // Transform Class to Bean
+    public MyFirstClass mySecondBean(){
+        return new  MyFirstClass("Second Bean");
+    }
+
 }
--- src/main/java/com/fouad/exemple/MyFirstService.java
+++ src/main/java/com/fouad/exemple/MyFirstService.java
@@ -7,10 +7,11 @@ import org.springframework.stereotype.Service;
 public class MyFirstService {
 
     private final MyFirstClass myFirstClass;
-    @Autowired // Use the needed Beans
+
     public MyFirstService(MyFirstClass myFirstClass) { //Constructor injection
         this.myFirstClass = myFirstClass;
     }
+
     public String tellStory(){
         return "the dependency is saying : " +myFirstClass.sayHello();
     }
