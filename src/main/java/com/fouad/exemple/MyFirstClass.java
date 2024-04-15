e7dd5fa7b20b0d7ab60fcabbdfca41897e5ec86d
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-15 12:53:25 +0100

    Automatic declaration of the Bean using the @Repository

--- src/main/java/com/fouad/exemple/MyFirstClass.java
+++ src/main/java/com/fouad/exemple/MyFirstClass.java
@@ -1,10 +1,12 @@
 package com.fouad.exemple;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 //@Component //Declare the class as Bean automaticaly
-@Service  // Declare the class as Bean automaticaly
+//@Service  // Declare the class as Bean automaticaly : @Service extend from component
+@Repository //Declare the class as Bean automaticaly
 public class MyFirstClass {
     public String sayHello(){
         return "Hello Fom first class";
