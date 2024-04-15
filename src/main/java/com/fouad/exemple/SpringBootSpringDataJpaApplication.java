869555a210c71389bd2f9f1d298374b7e24683c7
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-15 15:03:47 +0100

    Use profiles during the execution of the app

--- src/main/java/com/fouad/exemple/SpringBootSpringDataJpaApplication.java
+++ src/main/java/com/fouad/exemple/SpringBootSpringDataJpaApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class SpringBootSpringDataJpaApplication {
 
 	public static void main(String[] args) {
-		var ctx = SpringApplication.run(SpringBootSpringDataJpaApplication.class, args);
+
+		var app = new  SpringApplication(SpringBootSpringDataJpaApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
+
+		var ctx = app.run(args);
 
 		MyFirstService myFirstService = ctx.getBean(MyFirstService.class); //get the bean
 		System.out.println(myFirstService.tellStory());
