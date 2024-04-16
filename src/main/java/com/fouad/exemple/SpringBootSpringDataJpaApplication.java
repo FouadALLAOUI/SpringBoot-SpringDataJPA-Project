2afd763ec2c141c9fd83a5c7f24b7d07b4277f52
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-16 10:53:33 +0100

    Clean the project to start the Spring Rest

--- src/main/java/com/fouad/exemple/SpringBootSpringDataJpaApplication.java
+++ src/main/java/com/fouad/exemple/SpringBootSpringDataJpaApplication.java
@@ -10,16 +10,7 @@ import java.util.Collections;
 public class SpringBootSpringDataJpaApplication {
 
 	public static void main(String[] args) {
-
-		var app = new  SpringApplication(SpringBootSpringDataJpaApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active", "dev"));
-
-		var ctx = app.run(args);
-
-		MyFirstService myFirstService = ctx.getBean(MyFirstService.class); //get the bean
-		System.out.println(myFirstService.tellStory());
-		System.out.println(myFirstService.getCustomProperty());
-		System.out.println(myFirstService.getCustomPropertyInt());
+		SpringApplication.run(SpringBootSpringDataJpaApplication.class);
 
 
 	}
