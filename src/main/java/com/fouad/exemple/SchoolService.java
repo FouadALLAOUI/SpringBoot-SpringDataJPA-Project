419467860577fd5a940ab6f01a65d15fafe3b0e4
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-17 15:17:40 +0100

    Business logic in service layer : in SchoolController and SchoolService

---
+++ src/main/java/com/fouad/exemple/SchoolService.java
@@ -0,0 +1,35 @@
+package com.fouad.exemple;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SchoolService {
+
+    private final SchoolRepository schoolRepository; //inject repo
+    private final SchoolMapper schoolMapper; //inject mapper
+
+    public SchoolService(SchoolRepository schoolRepository, SchoolMapper schoolMapper) {
+        this.schoolRepository = schoolRepository;
+        this.schoolMapper = schoolMapper;
+    }
+
+    public SchoolDto create(@RequestBody SchoolDto dto){
+        var school = this.schoolMapper.toSchool(dto);
+        this.schoolRepository.save(school);
+        return dto;
+    }
+
+    public List<SchoolDto> findAll(){
+        return schoolRepository.findAll()
+                .stream()
+                .map(schoolMapper::toSchoolDto)
+                .collect(Collectors.toList());
+    }
+
+
+
+}
