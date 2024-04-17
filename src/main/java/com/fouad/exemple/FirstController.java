ca88297240cb9742c154c054858373d58d1d606c
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-17 11:40:04 +0100

    Create a repository and use jpa repo, create a controller to do some operations on student tab in db

--- src/main/java/com/fouad/exemple/FirstController.java
+++ src/main/java/com/fouad/exemple/FirstController.java
@@ -1,20 +1,39 @@
 package com.fouad.exemple;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class FirstController {
 
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello  From Controller";
+    private  final StudentRepository repository;
+
+    public FirstController(StudentRepository repository) {
+        this.repository = repository;
     }
 
-    @PostMapping("/post")
-    public String post(@RequestBody String message){
-        return "Request accepted an message : "+message;
+    @PostMapping("/students")
+    public Student post(@RequestBody Student student){
+        return  repository.save(student);
     }
+
+    @GetMapping("/students/{student-id}")
+    public Student findStudentById(@PathVariable("student-id") Integer id){
+        return  repository.findById(id).orElse(new Student());
+    }
+
+    @GetMapping("/students")
+    public List<Student> findAllStudent(){
+        return  repository.findAll();
+    }
+
+    @GetMapping("/students/search/{student-name}")
+    public List<Student> findStudentByName(@PathVariable("student-name") String name){
+        return  repository.findAllByFirstnameContaining(name);
+    }
+
+
 
 
 
---
+++ src/main/java/com/fouad/exemple/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.fouad.exemple;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    List<Student> findAllByFirstnameContaining(String p);
+
+}
