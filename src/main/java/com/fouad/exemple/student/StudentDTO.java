27f937d7b6a3d3391743cfe5b0c47b279df26071
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-18 12:12:31 +0100

    Use @NotEmpty and @Valid and @ExceptionHandler for validation

--- src/main/java/com/fouad/exemple/student/StudentDTO.java
+++ src/main/java/com/fouad/exemple/student/StudentDTO.java
@@ -1,8 +1,14 @@
 package com.fouad.exemple.student;
 
+
+import jakarta.validation.constraints.NotEmpty;
+
 public record StudentDTO(
+    @NotEmpty
     String firstname,
+    @NotEmpty
     String lastname,
+    @NotEmpty
     String email,
     Integer schoolId
 ) {
