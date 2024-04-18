a01fa4d5a4d648909065407e205bf49a7b0c8605
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-18 12:48:40 +0100

    @BeforeReach for testing

--- src/test/java/com/fouad/exemple/student/StudentMapperTest.java
+++ src/test/java/com/fouad/exemple/student/StudentMapperTest.java
@@ -1,14 +1,25 @@
 package com.fouad.exemple.student;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentMapperTest {
+
+    @BeforeEach // Run before running any test
+    void setUp() {
+        System.out.println("Before");
+    }
     @Test
     public void testMethod1(){
         System.out.println("My First test method");
+    }
+
+    @Test
+    public void testMethod2(){
+        System.out.println("My Second test method");
     }
 
 }
