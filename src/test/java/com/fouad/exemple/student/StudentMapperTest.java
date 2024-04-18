076545c03a468d1d7e250f9e12d8ad5f92c68586
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-18 12:45:45 +0100

    Create a test function in StudentMapperTest

--- src/test/java/com/fouad/exemple/student/StudentMapperTest.java
+++ src/test/java/com/fouad/exemple/student/StudentMapperTest.java
@@ -1,7 +1,14 @@
 package com.fouad.exemple.student;
 
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentMapperTest {
+    @Test
+    public void testMethod1(){
+        System.out.println("My First test method");
+    }
 
 }
